515786862cc259cf8813dc8633b5d200016a9c74
“zheng0116” <2387014174@qq.com>
2025-07-05 23:50:23 +0800

    fix: Modify the Dinov2 file name

--- retrieval/__init__.py
+++ retrieval/__init__.py
@@ -1,6 +1,6 @@
 from .services.utils.image_process import ImagePreprocessor
 from .services.retrieval import RetrievalProcessor
-from .services.model.Dinov2 import Dinov2
+from .services.model.dinov2 import Dinov2
 from .services.model.clip import Clip
 from .config.routes import Routes
 from .config.initialize import Initializer
